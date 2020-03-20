a53bbd249ed943571b88a044d50258f0588e9df9
maxkraft7 <49827312+maxkraft7@users.noreply.github.com>
2020-03-20 16:03:58 +0100

    initial commit

---
+++ src/main/java/net/imagej/Invert.java
@@ -0,0 +1,29 @@
+package net.imagej;
+
+import ij.*;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.*;
+
+public class Invert implements PlugInFilter {
+
+    public int setup(String arg, ImagePlus imp) {
+        if (arg.equals("about"))
+        {showAbout(); return DONE;}
+        return DOES_8G+DOES_STACKS+SUPPORTS_MASKING;
+    } //setup
+
+    public void run(ImageProcessor ip) {
+        byte[] pixels = (byte[])ip.getPixels();
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+
+        int[][] inDataArrInt = ImageJUtility.convertFrom1DByteArr(pixels, width, height);
+        int[] invertTF = ImageTransformationFilter.getInversionTF(255);
+        int[][] invertedImg = ImageTransformationFilter.getTransformedImage(inDataArrInt, width, height, invertTF);
+
+        ImageJUtility.showNewImage(invertedImg, width, height, "inverted image");
+
+    }
+
+}
+
